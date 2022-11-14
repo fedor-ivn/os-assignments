db25c9399f435503e3a1e6b18e03d6a376748dea
Fedor Ivanov <ivnfedor@gmail.com>
2022-11-14 08:55:31 +0000

    Add printing of filenames

--- week10/ex01.sh
+++ week10/ex01.sh
@@ -2,6 +2,6 @@ echo "Fedor Ivanov" > _ex1.txt
 ln _ex1.txt _ex1_1.txt
 ln _ex1.txt _ex1_2.txt
 for FILENAME in ./_ex1*.txt; do
-    stat -c %i $FILENAME | tee -a ex1.txt
+    ls -i $FILENAME | tee -a ex1.txt
 done
 
